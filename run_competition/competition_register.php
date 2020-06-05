f8d67fd2da9aaf49945326ecb8f6daf2c2a008ba
moconnell <moconnell@mimecast.com>
2020-06-05 13:58:48 -0400

    misc changes

--- run_competition/competition_register.php
+++ run_competition/competition_register.php
@@ -5,43 +5,44 @@ ck_testing();
 
 echo get_web_page_header(true, false, true);
 
+?>
+<p class="title"><u>NEOC club member registration:</u>
+<form action="./name_lookup.php">
+<p>Lookup by member name:
+<p>First name 
+<input type="text" name="competitor_first_name"><br>
+<p>Last name 
+<input type="text" name="competitor_last_name"><br>
+<input type="submit" value="Member name lookup">
+</form>
 
-echo "<h1>NEOC club member registration:</h1>\n";
-echo "<form action=\"./name_lookup.php\">\n";
-echo "<p>Lookup by member name:\n";
-echo "<p>First name \n";
-echo "<input type=\"text\" name=\"competitor_first_name\"><br>\n";
-echo "<p>Last name \n";
-echo "<input type=\"text\" name=\"competitor_last_name\"><br>\n";
-echo "<input type=\"submit\" value=\"Member name lookup\">\n";
-echo "</form>\n";
-
-echo "<form action=\"./stick_lookup.php\">\n";
-echo "<p>Lookup by Si Stick:\n";
-echo "<input type=\"text\" name=\"si_stick\"><br>\n";
-echo "<input type=\"submit\" value=\"SI stick lookup\">\n";
-echo "</form>\n";
+<form action="./stick_lookup.php">
+<p>Lookup by Si Stick:
+<input type="text" name="si_stick"><br>
+<input type="submit" value="SI stick lookup">
+</form>
 
 
-echo "<br><br><br><h1>Non-NEOC club member registration:</h1>\n";
-echo "<form action=\"./non_member.php\">\n";
-echo "<br><p>What is your name?<br>\n";
-echo "<p>First name \n";
-echo "<input type=\"text\" name=\"competitor_first_name\"><br>\n";
-echo "<p>Last name \n";
-echo "<input type=\"text\" name=\"competitor_last_name\"><br>\n";
-echo "<br><p>What is your orienteering club affiliation (if any)?<br>\n";
-echo "<input type=\"text\" name=\"club_name\"><br>\n";
-echo "<br><p>What is your SI stick number (if any)?<br>\n";
-echo "<input type=\"text\" name=\"si_stick\"><br>\n";
-echo "<br><p>What is your email (to send results link)?<br>\n";
-echo "<input type=\"text\" name=\"email\"><br>\n";
-echo "<br><p>What is your cell phone number (in case we need to contact you)?<br>\n";
-echo "<input type=\"text\" name=\"cell_number\"><br>\n";
-echo "<br><p>What car make/model did you come in (so we can see if you've left)?<br>\n";
-echo "<input type=\"text\" name=\"car_info\"><br>\n";
-echo "<input type=\"submit\" value=\"Submit\">\n";
-echo "</form>";
+<br><br><br><p class="title"><u>Non-NEOC club member registration:</u>
+<form action="./non_member.php">
+<p>What is your name?<br>
+<p>First name 
+<input type="text" name="competitor_first_name"><br>
+<p>Last name 
+<input type="text" name="competitor_last_name"><br>
+<br><p>What is your orienteering club affiliation (if any)?<br>
+<input type="text" name="club_name"><br>
+<br><p>What is your SI stick number (if any)?<br>
+<input type="text" name="si_stick"><br>
+<br><p>What is your email (to send results link)?<br>
+<input type="text" name="email"><br>
+<br><p>What is your cell phone number (in case we need to contact you)?<br>
+<input type="text" name="cell_number"><br>
+<br><p>What car make/model did you come in (so we can see if you've left)?<br>
+<input type="text" name="car_info"><br>
+<input type="submit" value="Submit">
+</form>
 
+<?php
 echo get_web_page_footer();
 ?>
