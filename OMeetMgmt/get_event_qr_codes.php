f70e1d989f72d7cb1a476bace378d567b92cc7a7
moconnell <moconnell@mimecast.com>
2020-09-22 13:59:16 -0400

    Reorganize listing of choices for clarity

--- OMeetMgmt/get_event_qr_codes.php
+++ OMeetMgmt/get_event_qr_codes.php
@@ -68,19 +68,22 @@ $add_event_string = "event={$event}";
 <p><p>
 <?php
 echo "<ul>\n";
-echo "<li><strong>On-course QR codes</strong>\n";
+echo "<li><strong>Non-reusable registration</strong> (for non-organized meet events (Bring Your Own Map))\n";
+echo "<ul>" .
+        "<li><input type=checkbox checked name=\"qr-BYOM registration\" value=\"{$url_prefix}/OMeetRegistration/register.php?{$add_key_string}&{$add_event_string}\">BYOM registration" .
+     "</ul>\n";
+echo "<li><strong>Registration QR codes</strong> (for ogranized meets, reusable at different venues)\n";
+echo "<ul>\n";
+echo "<li><input type=checkbox name=\"qr-Non-Member registration\" value=\"{$url_prefix}/OMeetWithMemberList/non_member.php?{$add_key_string}\">Non member registration\n";
+echo "<li><input type=checkbox name=\"qr-Member registration\" value=\"{$url_prefix}/OMeetWithMemberList/member.php?{$add_key_string}\">Member registration\n";
+echo "</ul>\n";
+echo "<li><strong>On-course QR codes</strong> (resuable across events/courses)\n";
 echo "<ul>\n";
 echo "<li><input type=checkbox name=\"qr-Start\" value=\"{$url_prefix}/OMeet/start_course.php\">Start\n";
 echo "<li><input type=checkbox name=\"qr-Finish\" value=\"{$url_prefix}/OMeet/finish_course.php\">Finish\n";
 foreach (array_keys($all_controls) as $one_control) {
   echo "<li><input type=checkbox name=\"qr-Control {$one_control}\" value=\"{$url_prefix}/OMeet/reach_control.php?control={$one_control}\">Control {$one_control}\n";
 }
-echo "</ul>\n";
-echo "<li><strong>Registration QR codes</strong>\n";
-echo "<ul>\n";
-echo "<li><input type=checkbox name=\"qr-Non-Member registration\" value=\"{$url_prefix}/OMeetWithMemberList/non_member.php?{$add_key_string}\">Non member registration\n";
-echo "<li><input type=checkbox name=\"qr-Member registration\" value=\"{$url_prefix}/OMeetWithMemberList/member.php?{$add_key_string}\">Member registration\n";
-echo "<li><input type=checkbox name=\"qr-BYOM registration\" value=\"{$url_prefix}/OMeetRegistration/register.php?{$add_key_string}&{$add_event_string}\">BYOM registration\n";
 echo "</ul>\n";
 echo "</ul>\n";
 ?>
