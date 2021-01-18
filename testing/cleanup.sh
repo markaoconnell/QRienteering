8731eff616223f1fc2f260921c5eb39a024d507f
moconnell <moconnell@mimecast.com>
2021-01-18 12:57:36 -0500

    Cleanup extra log files from python read_results

--- testing/cleanup.sh
+++ testing/cleanup.sh
@@ -6,6 +6,7 @@ if [ -d "${testing_directory}" ]
 then
   echo Removing ${testing_directory}
   rm -rf ${testing_directory}
+  rm -rf event-[0-9a-f]*-results.log
 else
   echo No directory found ${testing_directory}, no cleanup appears to be needed.
 fi
