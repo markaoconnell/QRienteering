9605d76db644ea933c3905078c6edbb381e9a88f
Mark OConnell <mark@mkoconnell.com>
2021-03-18 17:39:03 -0400

    Change button text to be clearer

--- OMeetMgmt/competitor_info.php
+++ OMeetMgmt/competitor_info.php
@@ -139,7 +139,7 @@ $time_limit_string .= "</ul>\n";
 $time_limit_string .= "<input type=hidden name=\"key\" value=\"${key}\">\n";
 $time_limit_string .= "<input type=hidden name=\"event\" value=\"${event}\">\n";
 $time_limit_string .= "<p>Include finished competitors? <input type=checkbox name=\"include_finishers\" value=\"1\"" . ($include_finishers ? " checked " : "")  . ">\n";
-$time_limit_string .= "<p><input type=submit value=\"Change filter\"></form>\n";
+$time_limit_string .= "<p><input type=submit value=\"Update competitor list\"></form>\n";
 
 
 $results_string = "<p>Competitors for {$event_name}<p><p>\n";
