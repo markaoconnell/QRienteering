65070f4423ee9821cecc57572e5c2b943d7c3242
Mark OConnell <mark@mkoconnell.com>
2021-08-30 12:14:25 -0400

    Modifications to read_results.py for useability

--- OMeetWithMemberList/read_results.py
+++ OMeetWithMemberList/read_results.py
@@ -2,6 +2,7 @@
 
 import sys, getopt
 import os
+import os.path
 import subprocess
 import re
 import time
@@ -25,7 +26,10 @@ from datetime import datetime
 verbose = 0
 debug = 0
 testing_run = 0
+continuous_testing = 0
 url = "http://www.mkoconnell.com/OMeet/not_there"
+fake_offline_event = "offline_downloads"
+use_fake_read_results = False
 
 # URLs for the web site
 VIEW_RESULTS = "OMeet/view_results.php"
@@ -183,13 +187,21 @@ def replay_stick_results(event_key, event, stick_to_replay):
 # So the si stick matches at the start of the line, adding the semicolon to eliminate partial matches
   found_stick = False
   stick_at_log_start = "{};".format(stick_to_replay)
-  with open("{}-results.log".format(event), "r") as LOGFILE:
+  processing_offline_results = (stick_to_replay == "offline")
+  
+  # read from either the event log or the offline log
+  if processing_offline_results:
+    logfile_name = fake_offline_event + "-results.log"
+  else:
+    logfile_name = "{}-results.log".format(event)
+
+  with open(logfile_name, "r") as LOGFILE:
     for result_line in LOGFILE:
       if debug:
         print ("Checking {}.".format(result_line))
 
-      if (result_line.startswith(stick_at_log_start)):
-        if debug:
+      if (processing_offline_results or result_line.startswith(stick_at_log_start)):
+        if debug and not processing_offline_results:
           print("Found match with {}.".format(stick_at_log_start))
 
         found_stick = True
@@ -233,6 +245,7 @@ def read_fake_results():
   if len(fake_entries) > 0:
     return fake_entries.pop()
   else:
+    sys.exit(0)
     return {SI_STICK_KEY : None}
 
 ###############################################################
@@ -286,10 +299,12 @@ def read_results(si_reader):
   card_type = si_reader.cardtype
 
 # read out card data
+  no_data = False
   try:
     card_data = si_reader.read_sicard()
   except SIReaderException as sire:
     print "Bad card ({}) download, error {}.".format(card_number, sire)
+    no_data = True
     return({SI_STICK_KEY : None})
 
 # beep
@@ -298,6 +313,11 @@ def read_results(si_reader):
 # Wait for the card to be removed from the reader
   while not si_reader.poll_sicard():
     time.sleep(1)
+
+  if no_data:
+    print "No data found on stick {}.".format(card_number)
+    return({SI_STICK_KEY : None})
+  
 
 # Convert to the format expected by the rest of the program
 # Check for old sticks which only use 12 hour time, which have some trouble if
@@ -311,23 +331,21 @@ def read_results(si_reader):
     finish_timestamp = get_24hour_timestamp(card_data['finish'])
   else:
     finish_timestamp = 0
+    print "No finish timestamp on stick {} - please scan finish and then download.".format(card_number)
 	
   array_of_punches = []
-  if ((finish_timestamp < start_timestamp) and (start_timestamp < TWELVE_HOURS_IN_SECONDS)):
+  if ((finish_timestamp < start_timestamp) and (finish_timestamp < TWELVE_HOURS_IN_SECONDS)):
     # Anomaly detected!  Adjust any timestamp less than the start forward by 12 hours
     # First convert the tuples of datetime objects to just a value in seconds
     # Then adjust the appropriate entries (those less than the start timestamp) by 12 hours
     # Then format it as : separated string items
-    #
-    # Update: I think the sireader code already handles this situation, so I don't think this case
-    # will ever occur.  Leaving this in, just in case though.
-    finish_timestamp += TWELVE_HOURS_IN_SECONDS
+    if (finish_timestamp != 0): finish_timestamp += TWELVE_HOURS_IN_SECONDS
     orig_punches = []
     new_punches = []
     orig_punches = map(lambda punch: (punch[0], get_24hour_timestamp(punch[1])), card_data['punches'])
     new_punches = map(lambda punch: (punch[0], punch[1] + TWELVE_HOURS_IN_SECONDS if (punch[1] < start_timestamp) else punch[1]), orig_punches)
     array_of_punches = map(lambda punch: "{}:{}".format(str(punch[0]), str(punch[1])), new_punches)
-    print "Old card detected and handled!"
+    if verbose: print "Adjusting some times for {} by twelve hours.".format(card_number)
   else:
     array_of_punches = map(lambda punch: "{}:{}".format(str(punch[0]), str(get_24hour_timestamp(punch[1]))), card_data['punches'])
 
@@ -337,50 +355,6 @@ def read_results(si_reader):
   
   return(entry_to_return)
 
-
-
-## Need to handle a reused stick - use stick-raw_start as the key???
-#sub read_results {
-#
-#  my($result_file);
-#  foreach $result_file (@result_files) {
-##	print "Opening $result_file to read results.\n" if ($debug);
-#    open(INFILE, "<$result_file");
-#    my(@result_lines) = <INFILE>;
-#    close(INFILE);
-#
-##    print "Read " . scalar(@result_lines) . " lines from $result_file.\n";
-#  
-#    chomp(@result_lines);
-#
-#    my($result_line);
-#    foreach $result_line (@result_lines) {
-#      $result_line =~ s/\r//g;
-#      my($stick, $raw_start, $raw_clear, @controls) = split(";", $result_line);
-#
-#      my($hash_key) = "${stick};" . get_timestamp($controls[0]);
-#
-#      if (!defined($results_by_stick{$hash_key})) {
-#        $new_results_by_stick{$hash_key} = \@controls;
-#      }
-#    }
-#  }
-#
-#}
-#
-#sub get_timestamp {
-#  my($or_time) = @_;
-#  my($week, $day, $hour, $minute, $second) = split(":", $or_time);
-#  my($timestamp) = 0;
-#
-#  $timestamp += ($hour * 3600) if ($hour ne "--");
-#  $timestamp += ($minute * 60) if ($minute ne "--");
-#  $timestamp += $second if ($second ne "--");
-#
-##  print "Converted ${or_time} to ${timestamp}.\n";
-#
-#  return($timestamp);
-#}
 
 ##################################################################
 def make_url_call(php_script_to_call, params):
@@ -418,7 +392,6 @@ event = ""
 event_name = ""
 event_key = ""
 serial_port = ""
-download_only = False
 event_found = False
 
 if ("key" in initializations):
@@ -442,7 +415,7 @@ if ("serial_port" in initializations):
 replay_si_stick = 0
 
 try:
-  opts, args = getopt.getopt(sys.argv[1:], "e:k:u:s:dvjtrh")
+  opts, args = getopt.getopt(sys.argv[1:], "e:k:u:s:fcdvtrh")
 except getopt.GetoptError:
   print("Parse error on command line.")
   usage()
@@ -460,10 +433,12 @@ for opt, arg in opts:
     url = arg
   elif opt == "-s":
     serial_port = arg
+  elif opt == "-f":
+    use_fake_read_results = True
+  elif opt == "-c":
+    continuous_testing = True
   elif opt == "-d":
     debug = 1
-  elif opt == "-j":
-    download_only = True
   elif opt == "-v":
     verbose = 1
   elif opt == "-t":
@@ -491,18 +466,36 @@ if (event == ""):
   event = result_tuple[0]
   print("Processing results for event {} ({}).".format(event_name, event))
 
+run_offline = False
 if ((event == "") or (event_key == "")):
-  if download_only:
-    event="download_only"
-    event_found = False
+  answer = raw_input("No event found - type yes to run in offline mode? ")
+  answer = answer.strip().lower()
+  run_offline = (answer == "yes")
+  if run_offline:
+    event = fake_offline_event
+    if os.path.exists(fake_offline_event + "-results.log"):
+      # If the offline log was last modified on a different day, then move it to a backup, thereby starting a new one
+      # otherwise just append to it, assuming it is from the same event
+      dlm_tuple = time.localtime(os.path.getmtime(fake_offline_event + "-results.log"))
+      today_tuple = time.localtime(None)
+      if not ((today_tuple.tm_year == dlm_tuple.tm_year) and (today_tuple.tm_mon == dlm_tuple.tm_mon)
+                 and (today_tuple.tm_mday == dlm_tuple.tm_mday)):
+        retry_count = 0
+        while retry_count < 1000:
+          backup_file = "{}-{}-{:02d}-{:02d}-{}-results.log".format(fake_offline_event, dlm_tuple.tm_year, dlm_tuple.tm_mon, dlm_tuple.tm_mday, retry_count)
+          if not os.path.exists(backup_file):
+            if verbose or debug: print "Backing up existing results file as {}".format(backup_file)
+            os.rename(fake_offline_event + "-results.log", backup_file)
+            break
+          retry_count += 1
+      else:
+        if verbose or debug: print "Appending results to {}-results.log".format(fake_offline_event)
   else:
     usage()
     sys.exit(1)
-else:
-  event_found = True
   
 ## Ensure that the event specified is valid
-if event_found:
+if not run_offline:
   output = make_url_call(VIEW_RESULTS, "event={}&key={}".format(event, event_key))
   if ((re.search("No such event found {}".format(event), output) != None) or (re.search(r"Show results for", output) == None)):
     print("Event {} not found, please check if event {} and key {} are valid.".format(event, event, event_key))
@@ -510,7 +503,7 @@ if event_found:
 
 
 if (replay_si_stick):
-  si_stick_to_replay = raw_input("Enter the si stick to replay: ")
+  si_stick_to_replay = raw_input("Enter the si stick to replay, or offline to reply offline downloads: ")
   si_stick_to_replay = si_stick_to_replay.strip()
   if (si_stick_to_replay == ""):
     print "ERROR: Must enter si stick when using the -r option, re-run program to try again."
@@ -534,6 +527,8 @@ loop_count = 0
 while True:
   if not testing_run:
     si_stick_entry = read_results(si_reader)
+  elif use_fake_read_results:
+    si_stick_entry = read_fake_results()
   else:
     si_stick_entry = { SI_STICK_KEY : None }
 
@@ -559,202 +554,25 @@ while True:
     with open("{}-results.log".format(event), "a") as LOGFILE:
       LOGFILE.write(qr_result_string + "\n")
 
-    if event_found:
-      results = upload_results(event_key, event, qr_result_string)
-      print "\n".join(results)
+    # If the finish is 0, then the finish wasn't scanned - we've logged it but don't upload the result.
+    # By editing the log file and replaying the SI stick, we can adjust the result afterwards if necessary.
+    # Though the easiest is to have the competitor go and scan finish and then download again.
+    if (si_stick_entry[SI_FINISH_KEY] != 0):
+      if not run_offline:
+        results = upload_results(event_key, event, qr_result_string)
+        print "\n".join(results)
+      else:
+        total_time = si_stick_entry[SI_FINISH_KEY] - si_stick_entry[SI_START_KEY]
+        hours = total_time / 3600
+        minutes = (total_time - (hours * 3600)) / 60
+        seconds = (total_time - (hours * 3600) - (minutes * 60))
+        print "Downloaded results for si_stick {}, time was {}h:{}m:{}s ({}).".format(si_stick_entry[SI_STICK_KEY], hours, minutes, seconds, total_time)
     else:
-      total_time = si_stick_entry[SI_FINISH_KEY] - si_stick_entry[SI_START_KEY]
-      hours = total_time / 3600
-      minutes = (total_time - (hours * 3600)) / 60
-      seconds = (total_time - (hours * 3600) - (minutes * 60))
-      print "Downloaded results for si_stick {}, time was {}h:{}m:{}s ({}).".format(si_stick_entry[SI_STICK_KEY], hours, minutes, seconds, total_time)
+      print "Splits downloaded but no finish time found - punch finish and download again."
 
-  if testing_run:
+  if testing_run and not continuous_testing:
     break   # While testing, no need to wait for more results
 
   time.sleep(1)
   loop_count += 1
 
-
-
-#while (1) {
-#
-#  
-#  foreach $key (keys(%new_results_by_stick)) {
-##    print "Found for $key: " . join(",", @{$new_results_by_stick{$key}}) . "\n";
-#    my(@controls) = @{$new_results_by_stick{$key}};
-#
-#    my($start, $finish) = ($controls[0], $controls[1]);
-#    my($start_timestamp) = get_timestamp($start);
-#    my($finish_timestamp) = get_timestamp($finish);
-#    # Some old si cards only store 12 hour time, which will wrap to 0 if the competitor
-#    # starts before noon and finishes after noon.  Look for that and fix it.
-#    my($old_si_stick_detected) = 0;
-#    if (($finish_timestamp < $start_timestamp) && ($start_timestamp < $TWELVE_HOURS_IN_SECONDS)) {
-#      $finish_timestamp += $TWELVE_HOURS_IN_SECONDS;
-#      $old_si_stick_detected = 1;
-#    }
-#
-#    my($i);
-#    my(@qr_controls) = ();
-#    for ($i = 2; $i < @controls; $i += 2) {
-#      my($control, $or_time) = ($controls[$i], $controls[$i + 1]);
-#      my($control_timestamp) = get_timestamp($or_time);
-#      if ($old_si_stick_detected && ($control_timestamp < $start_timestamp)) {
-#        $control_timestamp += $TWELVE_HOURS_IN_SECONDS;
-#      }
-#      push(@qr_controls, "${control}:${control_timestamp}");
-#    }
-#
-#    my($qr_result_string) = "${key}," . join(",", "start:${start_timestamp}","finish:${finish_timestamp}", @qr_controls);
-#    print "Got results for ${key}: ${qr_result_string}\n" if ($verbose);
-#    # Base64 encode for upload to the website
-#    #print "$qr_result_string\n";
-#    my($web_site_string) = encode_base64($qr_result_string);
-#    $web_site_string =~ s/\n//g;
-#    $web_site_string =~ s/=/%3D/g;
-#    my($si_stick, $start_time) = split(";", $key);
-#    if (!$replay_si_stick || ($si_stick eq $si_stick_to_replay)) {
-#      $output = make_url_call($FINISH_COURSE, "event=$event&key=$event_key&si_stick_finish=$web_site_string");
-#  
-#      print "\nResults for si_stick ${si_stick}:\n";
-#      if ($output =~ /(Cannot find.*)/) {
-#        print "$1\n";
-#      }
-#    
-#      if ($output =~ /(Results for:.*)<p>/) {
-#        print "$1\n";
-#      }
-#  
-#      if ($output =~ /(Second scan.*)/) {
-#        print "$1\n";
-#      }
-#  
-#      $results_by_stick{$key} = $new_results_by_stick{$key};
-#
-#      $si_stick_to_replay_found = 1 if ($replay_si_stick);
-#    }
-#  }
-#
-#  %new_results_by_stick = ();
-#
-#  last if ($testing_run || $replay_si_stick);  # For testing, no need to wait for more results
-#
-#  sleep(3);
-#  $loop_count++;
-#}
-#
-#if ($replay_si_stick && !$si_stick_to_replay_found) {
-#  print "\nERROR: No results found for SI stick $si_stick_to_replay.\n";
-#}
-
-
-
-
-## Find the OR event to manage - it should be the last event
-## The or_path variable should point to a valid directory
-#if (! -d $or_path) {
-#  print "ERROR: No such directory \"$or_path\", cannot read results from OR downloads.\n";
-#  exit 1;
-#}
-#
-#opendir(OR_DIR, "$or_path") || die "Cannot open OR directory $or_path";
-#my(@or_dir_contents) = readdir(OR_DIR);
-#close(OR_DIR);
-#print join(",", @or_dir_contents) . ": elements found in \"$or_path\".\n" if ($debug);
-#my(@or_events) = grep { /^[0-9]+$/ && -d "$or_path/$_" } @or_dir_contents;
-#my(@sorted_or_events) = sort { $a <=> $b } @or_events;
-#print join(",", @sorted_or_events) . ": sorted elements found in \"$or_path\".\n" if ($debug);
-#my($or_event_number) = $sorted_or_events[$#sorted_or_events];
-#print "Using OR event $or_event_number\n" if ($verbose);
-#
-#@result_files = ("$or_path/$or_event_number/results.csv", "$or_path/$or_event_number/resultslog.csv");
-#
-#my($si_stick_to_replay);
-#my($si_stick_to_replay_found) = 0;
-#if ($replay_si_stick) {
-#  print "Replay which si stick?\n";
-#  $si_stick_to_replay = <STDIN>;
-#  chomp($si_stick_to_replay);
-#}
-#
-#my($loop_count) = 0;
-#while (1) {
-#  read_results();
-#
-#  if (($loop_count % 20) == 0) {
-#    my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
-#                                                localtime(time);
-#
-#    printf "Awaiting new results at: %02d:%02d:%02d\r", $hour, $min, $sec;
-#  }
-#  print "Found new keys: " . join(",", keys(%new_results_by_stick)) . "\n" if ($verbose && scalar(keys(%new_results_by_stick) > 0));
-#  
-#  my($key);
-#  foreach $key (keys(%new_results_by_stick)) {
-##    print "Found for $key: " . join(",", @{$new_results_by_stick{$key}}) . "\n";
-#    my(@controls) = @{$new_results_by_stick{$key}};
-#
-#    my($start, $finish) = ($controls[0], $controls[1]);
-#    my($start_timestamp) = get_timestamp($start);
-#    my($finish_timestamp) = get_timestamp($finish);
-#    # Some old si cards only store 12 hour time, which will wrap to 0 if the competitor
-#    # starts before noon and finishes after noon.  Look for that and fix it.
-#    my($old_si_stick_detected) = 0;
-#    if (($finish_timestamp < $start_timestamp) && ($start_timestamp < $TWELVE_HOURS_IN_SECONDS)) {
-#      $finish_timestamp += $TWELVE_HOURS_IN_SECONDS;
-#      $old_si_stick_detected = 1;
-#    }
-#
-#    my($i);
-#    my(@qr_controls) = ();
-#    for ($i = 2; $i < @controls; $i += 2) {
-#      my($control, $or_time) = ($controls[$i], $controls[$i + 1]);
-#      my($control_timestamp) = get_timestamp($or_time);
-#      if ($old_si_stick_detected && ($control_timestamp < $start_timestamp)) {
-#        $control_timestamp += $TWELVE_HOURS_IN_SECONDS;
-#      }
-#      push(@qr_controls, "${control}:${control_timestamp}");
-#    }
-#
-#    my($qr_result_string) = "${key}," . join(",", "start:${start_timestamp}","finish:${finish_timestamp}", @qr_controls);
-#    print "Got results for ${key}: ${qr_result_string}\n" if ($verbose);
-#    # Base64 encode for upload to the website
-#    #print "$qr_result_string\n";
-#    my($web_site_string) = encode_base64($qr_result_string);
-#    $web_site_string =~ s/\n//g;
-#    $web_site_string =~ s/=/%3D/g;
-#    my($si_stick, $start_time) = split(";", $key);
-#    if (!$replay_si_stick || ($si_stick eq $si_stick_to_replay)) {
-#      $output = make_url_call($FINISH_COURSE, "event=$event&key=$event_key&si_stick_finish=$web_site_string");
-#  
-#      print "\nResults for si_stick ${si_stick}:\n";
-#      if ($output =~ /(Cannot find.*)/) {
-#        print "$1\n";
-#      }
-#    
-#      if ($output =~ /(Results for:.*)<p>/) {
-#        print "$1\n";
-#      }
-#  
-#      if ($output =~ /(Second scan.*)/) {
-#        print "$1\n";
-#      }
-#  
-#      $results_by_stick{$key} = $new_results_by_stick{$key};
-#
-#      $si_stick_to_replay_found = 1 if ($replay_si_stick);
-#    }
-#  }
-#
-#  %new_results_by_stick = ();
-#
-#  last if ($testing_run || $replay_si_stick);  # For testing, no need to wait for more results
-#
-#  sleep(3);
-#  $loop_count++;
-#}
-#
-#if ($replay_si_stick && !$si_stick_to_replay_found) {
-#  print "\nERROR: No results found for SI stick $si_stick_to_replay.\n";
-#}
