c16392415ed464fa5c251563bf30debdadf0d78a
Mark OConnell <mark@mkoconnell.com>
2022-02-23 08:52:18 -0500

    Add support on the backend for a mass start based on a SI stick read

--- OMeetMgmt/mass_start_courses.php
+++ OMeetMgmt/mass_start_courses.php
@@ -12,8 +12,21 @@ $started_array = array();
 $event = $_GET["event"];
 $key = $_GET["key"];
 
+if (isset($_GET["si_stick_time"])) {
+  $si_stick_start_time = $_GET["si_stick_time"];
+  $si_time_supplied = true;
+
+  if (($si_stick_start_time < 0) || ($si_stick_start_time > 86400)) {
+    error_and_exit("Malformatted start time of {$si_stick_start_time}, must be between 0 and 86400 (one day)\n");
+  }
+}
+else {
+  $si_time_supplied = false;
+  $si_stick_start_time = 0;
+}
+
 if (!key_is_valid($key)) {
-  error_and_exit("Unknown management key \"$key\", are you using an authorized link?\n");
+  error_and_exit("Unknown management key \"{$key}\", are you using an authorized link?\n");
 }
 
 set_timezone($key);
@@ -32,9 +45,12 @@ if (($event != "") && file_exists($event_path) && is_dir($courses_path)) {
   // Get the list of competitors who haven't started and are on one of the courses to mass_start
   // Generate a start time for the si stick users - these generally seem to be timestamps for the current day,
   // so ignore everything but the hour, minute, second for today and use that as the si stick start time
-  $mass_start_time = time();
-  $si_stick_mass_start_pieces = explode(":", strftime("%T", $mass_start_time));
-  $si_stick_start_time = ($si_stick_mass_start_pieces[0] * 3600) + ($si_stick_mass_start_pieces[1] * 60) + $si_stick_mass_start_pieces[0];
+  if (!$si_time_supplied) {
+    $mass_start_time = time();
+    $si_stick_mass_start_pieces = explode(":", strftime("%T", $mass_start_time));
+    $si_stick_start_time = ($si_stick_mass_start_pieces[0] * 3600) + ($si_stick_mass_start_pieces[1] * 60) + $si_stick_mass_start_pieces[0];
+  }
+
 
   $competitor_path = get_competitor_directory($event, $key, "..");
   $competitor_list = scandir($competitor_path);
@@ -47,13 +63,18 @@ if (($event != "") && file_exists($event_path) && is_dir($courses_path)) {
 
       if (!file_exists("{$competitor_path}/{$competitor}/controls_found/start")) {
         if (!file_exists("{$competitor_path}/{$competitor}/si_stick")) {
-          file_put_contents("{$competitor_path}/{$competitor}/controls_found/start", $mass_start_time);
+          if (!$si_time_supplied) {
+            $started_array[] = "{$competitor_name} on " . ltrim($course_for_competitor, "0..9-");
+            file_put_contents("{$competitor_path}/{$competitor}/controls_found/start", $mass_start_time);
+          }
         }
         else {
           file_put_contents("{$competitor_path}/{$competitor}/mass_si_stick_start", $si_stick_start_time);
-          file_put_contents("{$competitor_path}/{$competitor}/raw_mass_start_time", strftime("%T", $mass_start_time));
+          $started_array[] = "{$competitor_name} on " . ltrim($course_for_competitor, "0..9-");
+          if (!$si_time_supplied) {
+            file_put_contents("{$competitor_path}/{$competitor}/raw_mass_start_time", strftime("%T", $mass_start_time));
+          }
         }
-        $started_array[] = "{$competitor_name} on " . ltrim($course_for_competitor, "0..9-");
       }
       else {
         $already_started_array[] = "{$competitor_name} already on " . ltrim($course_for_competitor, "0..9-");
