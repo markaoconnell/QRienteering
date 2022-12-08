66f8a352ef3058cc5202659580b8299983b1c6e6
Mark OConnell <mark@mkoconnell.com>
2022-12-07 21:04:41 -0500

    Start a script to make creating new versions of manage_event easier

---
+++ OMeetWithMemberList/MeetSW/make_release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo Starting to build release version of manage_event.py
+PYTHONPATH="../../../sportident-python" pyinstaller -y manage_event.py
+
+if [ -d "./dist/manage_event" ]
+then
+  echo Build successful, installing in NEOC_SI_reader
+  mv  "../../NEOC_SI_reader/NEOC SI Reader data files/dist/manage_event" "../../NEOC_SI_reader/NEOC SI Reader data files/dist/manage_event.old"
+  mv  ./dist/manage_event "../../NEOC_SI_reader/NEOC SI Reader data files/dist/manage_event" 
+  rm -rf "../../NEOC_SI_reader/NEOC SI Reader data files/dist/manage_event.old"
+fi
