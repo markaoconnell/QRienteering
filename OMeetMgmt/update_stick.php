d011380b76a8fd4494e0e1b8125b7a8e16e4de52
Mark OConnell <mark@mkoconnell.com>
2023-01-18 11:34:34 -0500

    Make sure to clear the old xlation (if any) when updating the si stick)

--- OMeetMgmt/update_stick.php
+++ OMeetMgmt/update_stick.php
@@ -59,6 +59,9 @@ else {
         }
       }
     }
+    else {
+      clear_stick_xlation($event, $key, file_get_contents("{$competitor_path}/si_stick"));
+    }
 
     file_put_contents("{$competitor_path}/si_stick", $new_stick);
     put_stick_xlation($event, $key, $competitor_id, $new_stick);
